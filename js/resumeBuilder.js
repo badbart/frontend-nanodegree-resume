ad1b14f9351aa11f8425666bef4698d7c6c2668d
Tom <tom.zille@gmail.com>
2016-05-12 13:19:46 +0200

    Added all the needed Display Functions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,13 +4,13 @@ var bio = {
     "contacts": {
         "mobile": "0123456789",
         "email": "tom.zille@gmail.com",
-        "github": "https://github.com/badbart",
+        "github": "badbart",
         "location": "Lich, Germany"
     },
     "welcomeMessage": "Hello There, have a good Day",
-    "skills": ["JavaScript", "HTML", "CSS", "Python", "C#", "Windows Network and Domain Administration"],
+    "skills": ["JavaScript", "HTML", "CSS", "Python", "C#"],
     "biopic": "images/fry.jpg"
-}
+};
 
 var education = {
     "schools": [
@@ -35,11 +35,11 @@ var education = {
         {
             "title": "FED Nanodegree",
             "school": "Udacity",
-            "dates": 2016,
+            "date": 2016,
             "url": "http://www.udacity.com"
         }
     ]
-}
+};
 
 var work = {
     "jobs": [
@@ -52,13 +52,13 @@ var work = {
         },
         {
             "employer": "HLB",
-            "title": "Developer + IT Admin, Hauptbahnhof",
-            "location": "Frankfurt am Main",
+            "title": "Developer + IT Admin",
+            "location": "Frankfurt am Main, Hauptbahnhof",
             "dates": "in progress",
             "description": "Same as before but more developing and planning"
         }
     ]
-}
+};
 
 var projects = {
     "projects": [
@@ -79,26 +79,118 @@ var projects = {
             ]
         }
     ]
-}
+};
+
+bio.display = function () {
+    var header = HTMLheaderName.replace("%data%", bio.name) + HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").prepend(header);
+
+    var contacts = HTMLmobile.replace("%data%", bio.contacts.mobile) +
+        HTMLemail.replace("%data%", bio.contacts.email) +
+        HTMLgithub.replace("%data%", bio.contacts.github) +
+        HTMLlocation.replace("%data%", bio.contacts.location);
+    $("#topContacts").append(contacts);
+
+    var picAndMessage = HTMLbioPic.replace("%data%", bio.biopic) + HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    $("#header").append(picAndMessage);
+
+    $("#header").append(HTMLskillsStart);
+
+    var skills = "";
+
+    $.each(bio.skills, function (i, val) {
+        skills = skills + HTMLskills.replace("%data%", val);
+    });
+
+
+    $("#skills").append(skills);
+
+    // Because on the Image it looks like this and changing it directly in CSS would
+    // be to easy
+    $("#skills").css("flex-direction", "column");
+
+
+};
+
+work.display = function () {
+    $.each(work.jobs, function (i, val) {
+        $("#workExperience").append(HTMLworkStart);
+
+        var employer = HTMLworkEmployer.replace("%data%", val.employer);
+        var title = HTMLworkTitle.replace("%data%", val.title);
+        var dates = HTMLworkDates.replace("%data%", val.dates);
+        var workLocation = HTMLworkLocation.replace("%data%", val.location);
+        var description = HTMLworkDescription.replace("%data%", val.description);
+
+        $(".work-entry:last").append(employer + title + dates + workLocation + description);
+
+    });
+};
 
 projects.display = function () {
-    for (project in projects.projects) {
+    $.each(projects.projects, function (i, val) {
         $("#projects").append(HTMLprojectStart);
-        var proTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-        var proDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-        var proDesc = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        var proTitle = HTMLprojectTitle.replace("%data%", val.title);
+        var proDates = HTMLprojectDates.replace("%data%", val.dates);
+        var proDesc = HTMLprojectDescription.replace("%data%", val.description);
 
         var pageHtml = proTitle + proDates + proDesc;
 
-        for (proImgs in projects.projects[project].images) {
-            var proImg = HTMLprojectImage.replace("%data%", projects.projects[project].images[proImgs]);
+        $.each(val.images, function (j, img) {
+            var proImg = HTMLprojectImage.replace("%data%", img);
             pageHtml = pageHtml + proImg;
-        }
+        });
 
-        $(".project-entry:last").append(pageHtml)
-    }
+        $(".project-entry:last").append(pageHtml);
+    });
+};
 
+education.display = function () {
+    $.each(education.schools, function (i, val) {
+        $("#education").append(HTMLschoolStart);
+
+        var schoolName = HTMLschoolName.replace("%data%", val.name);
+        var schoolDegree = HTMLschoolDegree.replace("%data%", val.degree);
+        var schoolDates = HTMLschoolDates.replace("%data%", val.dates);
+        var schoolLocation = HTMLschoolLocation.replace("%data%", val.location);
+
+        var schoolMajor = "";
+        $.each(val.majors, function (j, maj) {
+            schoolMajor = schoolMajor + HTMLschoolMajor.replace("%data%", maj);
+        });
+
+        var pageHtml = schoolName + schoolDegree + schoolDates + schoolLocation + schoolMajor;
+
+        $(".education-entry:last").append(pageHtml);
+    });
+
+    $("#education").append(HTMLonlineClasses);
+
+    $.each(education.onlineCourses, function (i, val) {
+        $("#education").append(HTMLschoolStart);
+
+        var onlineTitle = HTMLschoolName.replace("%data%", val.title);
+        var onlineSchool = HTMLschoolDegree.replace("%data%", val.school);
+        var onlineDates = HTMLschoolDates.replace("%data%", val.date);
+        var onlineUrl = HTMLonlineURL.replace("%data%", val.url);
+
+
+
+        var pageHtml = onlineTitle + onlineSchool + onlineDates + onlineUrl;
+
+        $(".education-entry:last").append(pageHtml);
+    });
+};
+
+
+function displayPage() {
+    bio.display();
+    work.display();
+    projects.display();
+    education.display();
+
+    $("#mapDiv").append(googleMap);
 }
 
-
+$(displayPage());
 
